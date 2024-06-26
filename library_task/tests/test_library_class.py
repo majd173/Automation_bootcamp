7fcb38dd9a620b715845e3123f367b96f9269aa5
majd173 <majdb173@gmail.com>
2024-06-26 15:33:07 +0300

    upload python to check

--- library_task/tests/test_library_class.py
+++ library_task/tests/test_library_class.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import unittest
 from library_task.Personal_Library_Manager import MyLibrary, Book
 from library_task.App import library_file_path
@@ -6,19 +7,18 @@ from library_task.App import library_file_path
 
 class TestLibraryClass(unittest.TestCase):
 
-    # def setUp(self):
-    #     self.library = MyLibrary("Test Library")
-    #     self.library_file_path = "test_library.json"
-    #
-    # def tearDown(self):
-    #     if os.path.exists(self.library_file_path):
-    #         os.remove(self.library_file_path)
+    def setUp(self):
+        self.library = MyLibrary("Test Library")
+        self.library_file_path = "test_library.json"
+
+    def tearDown(self):
+        if os.path.exists(self.library_file_path):
+            os.remove(self.library_file_path)
 
     def test_book_exist(self):
         book = Book("majd", 555, "bader", "drama", 8888, "aaaa")
-        library = MyLibrary.add_book(book)
-        library1 = MyLibrary.load_library(library_file_path)
-        self.assertIn(book, library1, "book does not found")
+        loaded_library = MyLibrary.load_library(self.library_file_path)
+        self.assertIn(book.__dict__, [b.__dict__ for b in loaded_library.books], "Not found")
 
 
 if __name__ == '__main__':
