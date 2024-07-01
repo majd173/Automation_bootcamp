0d784937cbaea71d993ea6813df5d1e4d52202e1
majd173 <majdb173@gmail.com>
2024-07-01 19:27:43 +0300

    google test selenium

--- intro_to_selenium/google_task.py
+++ intro_to_selenium/google_task.py
@@ -1,4 +1,3 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -7,15 +6,16 @@ from selenium.webdriver.common.keys import Keys
 driver = webdriver.Chrome()  # Chrome() is a constructor that initializes (opens) a chrome web-browser
 
 # infra    # logic
-driver.get('https://www.google.com/')
+driver.get('https://www.google.com')
 
 # logic
 search_input = driver.find_element(By.XPATH, '//*[@id="APjFqb"]')
 search_input.send_keys("Python programing", Keys.RETURN)
 new_input = driver.find_element(By.XPATH, "//span[@dir = 'ltr']")
-print(new_input.text)
+print("the first link is: ", new_input.text)
 print("Test passed")
 # search_input.send_keys(Keys.RETURN) # pressing enter
+
 # infra
 driver.quit()
 
