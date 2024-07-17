7c2099fc80486611c09b39d775629af7f57f29b9
majd173 <majdb173@gmail.com>
2024-07-18 01:05:16 +0300

    SolarSystemScope_project_part_1

--- API_Project/deck_of_cards/logic/deck_of_cards_web.py
+++ API_Project/deck_of_cards/logic/deck_of_cards_web.py
@@ -1,19 +1,30 @@
+import logging
 from API_Project.deck_of_cards.infra.API_Wrapper import APIWrapper
 from API_Project.deck_of_cards.infra.config_provider import ConfigProvider
+from API_Project.deck_of_cards.infra.logger_setup import LoggingSetup
 
 
 class APIDecksOfCards:
 
     def __init__(self, request: APIWrapper):
-        self._request = request
-        self._api = APIWrapper()
-        self._config = ConfigProvider().load_from_file("../cards_config.json")
+        try:
+            self._request = request
+            self._api = APIWrapper()
+            self._config = ConfigProvider().load_from_file("../cards_config.json")
+            self._url = self._config['base_url']
+        except ImportError:
+            logging.error("Can not open card_config.json file.")
+
+
 
     # --------------------------------------------------------------------------------------
     # GET REQUEST
     def get_back_of_card(self):
-        return self._request.get_request(
-            f'{self._config['base_url']}/static/img/back.png')
+        try:
+            return self._request.get_request(
+                f'{self._url}/static/img/back.png')
+        except Exception:
+            logging.error("Can not get a request.")
 
     def get_back_of_card_get_status(self):
         get_back_of_card_status = self.get_back_of_card().status_code
@@ -23,7 +34,7 @@ class APIDecksOfCards:
     # GET REQUEST
     def shuffle_the_cards(self):
         return self._request.get_request(
-            f'{self._config['base_url']}/api/deck/new/shuffle/?deck_count=1')
+            f'{self._url}/api/deck/new/shuffle/?deck_count=1')
 
     def shuffle_the_cards_get_json(self):
         shuffle_the_cards_json = self.shuffle_the_cards().json()
@@ -38,7 +49,7 @@ class APIDecksOfCards:
     # POST REQUEST
     def draw_a_card(self):
         return self._request.post_request(
-            f'{self._config['base_url']}/api/deck/new/draw/?count=2')
+            f'{self._url}/api/deck/new/draw/?count=2')
 
     def draw_a_card_get_json(self):
         draw_a_card_json = self.draw_a_card().json()
@@ -53,7 +64,7 @@ class APIDecksOfCards:
     # GET REQUEST
     def a_partial_deck(self):
         return self._request.get_request(
-            f'{self._config['base_url']}/api/deck/new/shuffle/?cards={self._config["cards"]}')
+            f'{self._url}/api/deck/new/shuffle/?cards={self._config["cards"]}')
 
     def a_partial_deck_get_json(self):
         a_partial_deck_json = self.a_partial_deck().json()
--- API_Project/deck_of_cards/tests/test_deck_of_cards.py
+++ API_Project/deck_of_cards/tests/test_deck_of_cards.py
@@ -1,5 +1,7 @@
 import unittest
 import pytest
+import logging
+from API_Project.deck_of_cards.infra.logger_setup import LoggingSetup
 from API_Project.deck_of_cards.infra.config_provider import ConfigProvider
 from API_Project.deck_of_cards.logic.deck_of_cards_web import APIDecksOfCards
 from API_Project.deck_of_cards.infra.API_Wrapper import APIWrapper
@@ -12,18 +14,22 @@ class TestDeckOfCards(unittest.TestCase):
         self._config = ConfigProvider().load_from_file("../cards_config.json")
         self._api = APIWrapper()
 
+
     def test_back_of_cards(self):
+        logging.info("_______Test (test_back_of_cards) Began_______")
         deck_of_cards = APIDecksOfCards(self._api)
         self.assertTrue(deck_of_cards.get_back_of_card())
         self.assertGreaterEqual(200, deck_of_cards.get_back_of_card_get_status())
 
     def test_shuffle_the_cards(self):
+        logging.info("_______Test (test_shuffle_the_cards) Began_______")
         deck_of_cards = APIDecksOfCards(self._api)
         self.assertTrue(deck_of_cards.shuffle_the_cards().ok)
         self.assertEqual(deck_of_cards.shuffle_the_cards_get_status(), 200)
         self.assertEqual(deck_of_cards.shuffle_the_cards_get_json(), 52)
 
     def test_draw_a_card(self):
+        logging.info("_______Test (test_draw_a_card) Began_______")
         deck_of_cards = APIDecksOfCards(self._api)
         self.assertTrue(deck_of_cards.draw_a_card().ok)
         self.assertEqual(deck_of_cards.draw_a_card_get_status(), 200)
@@ -31,6 +37,7 @@ class TestDeckOfCards(unittest.TestCase):
 
 
     def test_a_partial_deck(self):
+        logging.info("_______Test (test_a_partial_deck) Began_______")
         deck_of_cards = APIDecksOfCards(self._api)
         self.assertTrue(deck_of_cards.a_partial_deck().ok)
         self.assertEqual(deck_of_cards.a_partial_deck_get_status(), 200)
