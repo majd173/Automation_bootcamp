c275930148aafe89f73525a4163667d0e4d5aec0
majd173 <majdb173@gmail.com>
2024-07-18 09:12:13 +0300

    SolarSystemScope_project_part_1

--- API_Project/deck_of_cards/logic/deck_of_cards_web.py
+++ API_Project/deck_of_cards/logic/deck_of_cards_web.py
@@ -27,51 +27,86 @@ class APIDecksOfCards:
             logging.error("Can not get a request.")
 
     def get_back_of_card_get_status(self):
-        get_back_of_card_status = self.get_back_of_card().status_code
-        return get_back_of_card_status
-
+        try:
+            get_back_of_card_status = self.get_back_of_card().status_code
+            logging.info("Status request have been got.")
+            return get_back_of_card_status
+        except Exception:
+            logging.error("Can not get status request.")
     #--------------------------------------------------------------------------------------
     # GET REQUEST
     def shuffle_the_cards(self):
-        return self._request.get_request(
-            f'{self._url}/api/deck/new/shuffle/?deck_count=1')
+        try:
+            return self._request.get_request(
+                f'{self._url}/api/deck/new/shuffle/?deck_count=1')
+        except Exception:
+            logging.error("Can not get a request.")
 
     def shuffle_the_cards_get_json(self):
-        shuffle_the_cards_json = self.shuffle_the_cards().json()
-        data = shuffle_the_cards_json
-        return data['remaining']
+        try:
+            shuffle_the_cards_json = self.shuffle_the_cards().json()
+            data = shuffle_the_cards_json
+            logging.info("json request have been got.")
+            return data['remaining']
+        except Exception:
+            logging.error("Can not get json request.")
 
     def shuffle_the_cards_get_status(self):
-        shuffle_the_cards_status = self.shuffle_the_cards().status_code
-        return shuffle_the_cards_status
-
+        try:
+            shuffle_the_cards_status = self.shuffle_the_cards().status_code
+            logging.info("Status request have been got.")
+            return shuffle_the_cards_status
+        except Exception:
+            logging.error("Can not get status request.")
     #--------------------------------------------------------------------------------------
     # POST REQUEST
     def draw_a_card(self):
-        return self._request.post_request(
-            f'{self._url}/api/deck/new/draw/?count=2')
+        try:
+            return self._request.post_request(
+                f'{self._url}/api/deck/new/draw/?count=2')
+        except Exception:
+            logging.error("Can not post a request.")
 
     def draw_a_card_get_json(self):
-        draw_a_card_json = self.draw_a_card().json()
-        data = draw_a_card_json
-        return data['remaining']
+        try:
+            draw_a_card_json = self.draw_a_card().json()
+            data = draw_a_card_json
+            logging.info("json request have been got.")
+            return data['remaining']
+        except Exception:
+            logging.error("Can not get json request.")
 
     def draw_a_card_get_status(self):
-        draw_a_card_status = self.draw_a_card().status_code
-        return draw_a_card_status
+        try:
+            draw_a_card_status = self.draw_a_card().status_code
+            logging.info("Status request have been got.")
+            return draw_a_card_status
+        except Exception:
+            logging.error("Can not get status request.")
 
     #--------------------------------------------------------------------------------------
     # GET REQUEST
     def a_partial_deck(self):
-        return self._request.get_request(
-            f'{self._url}/api/deck/new/shuffle/?cards={self._config["cards"]}')
+        try:
+            return self._request.get_request(
+                f'{self._url}/api/deck/new/shuffle/?cards={self._config["cards"]}')
+        except Exception:
+            logging.error("Can not get a request.")
 
     def a_partial_deck_get_json(self):
-        a_partial_deck_json = self.a_partial_deck().json()
-        data = a_partial_deck_json
-        return data['remaining']
+        try:
+            a_partial_deck_json = self.a_partial_deck().json()
+            data = a_partial_deck_json
+            logging.info("json request have ben got.")
+            return data['remaining']
+        except Exception:
+            logging.error("Can not get json request.")
 
     def a_partial_deck_get_status(self):
-        a_partial_deck_status = self.a_partial_deck().status_code
-        return a_partial_deck_status
+        try:
+            a_partial_deck_status = self.a_partial_deck().status_code
+            logging.info("Status request have ben got.")
+            return a_partial_deck_status
+        except Exception:
+            logging.error("Can not get status request.")
     #--------------------------------------------------------------------------------------
