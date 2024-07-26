348194e30f6e9755f7a758457748862e8068926d
majd173 <majdb173@gmail.com>
2024-07-26 22:29:06 +0300

    Jira update & Pet Store data base update.

--- api/pet_store/infra/logger_setup.py
+++ api/pet_store/infra/logger_setup.py
@@ -1,10 +1,10 @@
 import logging
 
+
 class LoggingSetup():
     # This class manages a logging file that catches and records
     # important scenarios during tests running.
-
-    logging.basicConfig(filename="../pet_logfile.log",
+    logging.basicConfig(filename="./pet_logfile.log",
                         level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s:',
                         force=True)
 
